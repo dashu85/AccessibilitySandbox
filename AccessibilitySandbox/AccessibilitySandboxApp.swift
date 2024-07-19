c3cf052c2bdd6b80e750e8034df8d0637b0840ba
Marcus Benoit <benoit.marcus@gmail.com>
2024-07-19 11:30:12 +0200

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Marcus Benoit on 19.07.24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
